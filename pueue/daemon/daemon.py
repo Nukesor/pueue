2793e9f32faaf0fb8c205bd9e45d6fe6a09e9f96
Arne Beer <arne@twobeer.de>
2015-09-14 01:14:30 +0200

    Unable socket recv blocking

--- pueue/daemon/daemon.py
+++ pueue/daemon/daemon.py
@@ -11,6 +11,7 @@ def daemonMain():
     try:
         daemon = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
         daemon.bind(socketPath)
+        daemon.setblocking(0)
     except:
         print("Daemon couldn't bind to socket. Aborting")
         sys.exit(1)
@@ -18,21 +19,26 @@ def daemonMain():
         print("Daemon got socket")
 
     while True:
-        data = daemon.recv(1024)
-        command = pickle.loads(data)
-        if command['mode'] == 'add':
-            queue = readQueue()
-            print(queue)
-            if len(queue) != 0:
-                nextKey = max(queue.keys()) + 1
-            else:
-                nextKey = 0
-            queue[nextKey] = command
-            writeQueue(queue)
-        elif command['mode'] == 'EXIT':
-            print('Exiting')
-            break
-        time.sleep(1)
+        try:
+            data = daemon.recv(1024)
+        except BlockingIOError:
+            data = False
+
+        if data:
+            command = pickle.loads(data)
+            if command['mode'] == 'add':
+                queue = readQueue()
+                if len(queue) != 0:
+                    nextKey = max(queue.keys()) + 1
+                else:
+                    nextKey = 0
+                queue[nextKey] = command
+                writeQueue(queue)
+            elif command['mode'] == 'EXIT':
+                print('Shutting down pueue daemon')
+                break
+
+        time.sleep(0.5)
 
     os.remove(socketPath)
 
