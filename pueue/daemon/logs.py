29f4f298f420aa31a73924057b98c85bebc1e633
Arne Beer <arne@twobeer.de>
2015-12-26 20:25:44 +0100

    Log color formatting, don't print stdout, if there is no stdout

--- pueue/daemon/logs.py
+++ pueue/daemon/logs.py
@@ -1,6 +1,8 @@
 import os
 import time
 
+from colorclass import Color
+
 
 def writeLog(logDir, log, rotate):
     # Get path  for logfile
@@ -20,11 +22,41 @@ def writeLog(logDir, log, rotate):
     # Format and print Output
     for key in log:
         try:
-            logFile.write('Command #{} exited with returncode {}:  "{}" \n'.format(key, log[key]['returncode'], log[key]['command']))
+            # Get returncode color:
+            returncode = log[key]['returncode']
+            if returncode == 0:
+                returncode = Color('{autogreen}' + '{}'.format(returncode) + '{/autogreen}')
+            else:
+                returncode = Color('{autored}' + '{}'.format(returncode) + '{/autored}')
+
+            # Command Id with returncode
+            logFile.write(
+                Color(
+                    '{autoyellow}' +
+                    'Command #{} '.format(key) +
+                    '{/autoyellow}'
+                ) +
+                'exited with returncode {}: '.format(returncode) +
+                '"{}" \n'.format(log[key]['command'])
+            )
             logFile.write('Path: {} \n'.format(log[key]['path']))
             if log[key]['stderr']:
-                logFile.write('Stderr output: \n    {}\n'.format(log[key]['stderr']))
-            logFile.write('Stdout output: \n    {}'.format(log[key]['stdout']))
+                logFile.write(
+                    Color(
+                        '{autored}' +
+                        'Stderr output: ' +
+                        '{/autored}'
+                    ) +
+                    ' \n    {}\n'.format(log[key]['stderr'])
+                )
+            if len(log[key]['stdout']) > 0:
+                logFile.write(
+                    Color(
+                        '{autogreen}' +
+                        'Stdout output: ' +
+                        '{/autogreen}'
+                    ) +
+                    '\n    {}'.format(log[key]['stdout']))
             logFile.write('\n')
         except:
             print('Errored while writing to log file. Wrong file permissions?')
