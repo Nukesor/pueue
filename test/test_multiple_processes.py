f25e61691ab3114ff9ef89b774218b33b9a3301b
Arne Beer <arne@twobeer.de>
2017-02-12 13:50:19 +0100

    Test multiple processes test

--- test/test_multiple_processes.py
+++ test/test_multiple_processes.py
@@ -6,24 +6,25 @@ from test.helper import (
 )
 
 
-@pytest.mark.skip(reason="Needs to be adjusted to new multi process style")
 def test_multiple_spawned(daemon_setup):
     """Pause the daemon."""
 
-    # Add sleep command
-    status = command_factory('config')({
+    # Set max processes to three concurrent processes
+    command_factory('config')({
         "option": "maxProcesses",
         "value": 3,
     })
+    # Add sleep commands
     execute_add('sleep 60')
     execute_add('sleep 60')
     execute_add('sleep 60')
     execute_add('sleep 60')
     # Pause it with `'wait': True`
     # The paused daemon should wait for the process to finish
+    status = command_factory('status')()
     assert status['status'] == 'running'
     assert status['data'][0]['status'] == 'running'
     assert status['data'][1]['status'] == 'running'
     assert status['data'][2]['status'] == 'running'
-    assert status['data'][0]['status'] == 'queued'
+    assert status['data'][3]['status'] == 'queued'
 
