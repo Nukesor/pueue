eb1332683815fa6cdab964819eae29559587a6a4
Arne Beer <arne@twobeer.de>
2017-02-18 15:55:52 +0100

    Version bump 0.7.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pueue',
     author='Arne Beer',
     author_email='arne@twobeer.de',
-    version='0.7.4',
+    version='0.7.5',
     description='Pueue is a fancy queue for shell commands',
     keywords='shell queue command concurrent',
     url='http://github.com/nukesor/pueue',
