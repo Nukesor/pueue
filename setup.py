d0fe94205e5f5eaf61450b8416b717b97443d43a
Arne Beer <arne@twobeer.de>
2017-03-30 22:35:54 +0200

    Version pump 0.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pueue',
     author='Arne Beer',
     author_email='arne@twobeer.de',
-    version='0.8.0',
+    version='0.8.1',
     description='Pueue is a fancy queue for shell commands',
     keywords='shell queue command concurrent',
     url='http://github.com/nukesor/pueue',
