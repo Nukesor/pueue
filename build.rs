ce25516bef6dea22c0073c81ec5d1c0c5a65c1c5
Arne Beer <contact@arne.beer>
2020-02-02 12:59:44 +0100

    Resolves #60

--- build.rs
+++ build.rs
@@ -1,8 +1,15 @@
 use ::structopt::clap::Shell;
+use ::version_check;
 
 include!("client/cli.rs");
 
 fn main() {
+    if !version_check::is_min_version("1.39.0").unwrap_or(false) {
+        eprintln!("Pueue needs to be build with Rust version >=1.39");
+        eprintln!("Please update your rust version to stable.");
+        std::process::exit(1);
+    }
+
     let mut clap = Opt::clap();
 
     let completion_dir = "./utils/completions";
