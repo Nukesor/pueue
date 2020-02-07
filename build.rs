11fd859653a1857ca8f5ec710adbf014cdd08188
Arne Beer <contact@arne.beer>
2020-02-07 11:20:05 +0100

    Remove completion files from git

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use ::std::fs::create_dir_all;
+use ::std::path::Path;
 use ::structopt::clap::Shell;
 use ::version_check;
 
@@ -12,11 +14,17 @@ fn main() {
 
     let mut clap = Opt::clap();
 
-    let completion_dir = "./utils/completions";
+    let completion_path = "./utils/completions";
+    let completion_dir = Path::new(completion_path).to_path_buf();
+    // Create the config dir, if it doesn't exist yet
+    if !completion_dir.exists() {
+        create_dir_all(&completion_dir).expect("Couldn't create .utils/completion directory");
+    }
+
     let bin_name = "pueue";
-    clap.gen_completions(bin_name, Shell::Bash, completion_dir);
-    clap.gen_completions(bin_name, Shell::Fish, completion_dir);
-    clap.gen_completions(bin_name, Shell::PowerShell, completion_dir);
-    clap.gen_completions(bin_name, Shell::Elvish, completion_dir);
-//    clap.gen_completions(bin_name, Shell::Zsh, completion_dir);
+    clap.gen_completions(bin_name, Shell::Bash, completion_path);
+    clap.gen_completions(bin_name, Shell::Fish, completion_path);
+    clap.gen_completions(bin_name, Shell::PowerShell, completion_path);
+    clap.gen_completions(bin_name, Shell::Elvish, completion_path);
+//    clap.gen_completions(bin_name, Shell::Zsh, completion_path);
 }
