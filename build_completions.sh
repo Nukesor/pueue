07e59f86588725a85dd74ee1e078c7f21ad8cc25
Arne Beer <contact@arne.beer>
2020-03-25 19:28:28 +0100

    Run completion script in release --locked mode

--- build_completions.sh
+++ build_completions.sh
@@ -1,7 +1,7 @@
 mkdir -p utils/completions
-cargo run --bin pueue completions bash ./utils/completions
-cargo run --bin pueue completions fish ./utils/completions
-cargo run --bin pueue completions powershell ./utils/completions
-cargo run --bin pueue completions elvish ./utils/completions
+cargo run --release --locked --bin pueue completions bash ./utils/completions
+cargo run --release --locked --bin pueue completions fish ./utils/completions
+cargo run --release --locked --bin pueue completions powershell ./utils/completions
+cargo run --release --locked --bin pueue completions elvish ./utils/completions
 # Broken for now -.-
-#cargo run --bin pueue completions zsh ./utils/completions
+#cargo run --release --locked --bin pueue completions zsh ./utils/completions
