19f96c675c973a8e6ba6e5c5ca94fc96a52f6db9
Arne Beer <contact@arne.beer>
2020-08-15 16:36:06 +0200

    Fix windows clippy issues

--- daemon/platform/windows/process_helper.rs
+++ daemon/platform/windows/process_helper.rs
@@ -3,16 +3,16 @@ use anyhow::{bail, Result};
 use crate::task_handler::ProcessAction;
 
 /// Send a signal to a windows process.
-pub fn send_signal(pid: u32, action: &ProcessAction, children: bool) -> Result<bool> {
+pub fn send_signal(_pid: u32, _action: &ProcessAction, _children: bool) -> Result<bool> {
     bail!("not supported on windows.")
 }
 
 /// Get all children of a specific process.
 /// The Vec<i32> is just a placeholder.
-pub fn get_children(pid: i32) -> Option<Vec<i32>> {
+pub fn get_children(_pid: i32) -> Option<Vec<i32>> {
     None
 }
 
 /// Send a signal to multiple processes.
 /// The Vec<i32> is just a placeholder.
-pub fn send_signal_to_processes(processes: Vec<i32>, action: &ProcessAction) {}
+pub fn send_signal_to_processes(_processes: Vec<i32>, _action: &ProcessAction) {}
