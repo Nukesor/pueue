45713cab3067f64fc73d7ece4b7212a577cc1046
Arne Beer <contact@arne.beer>
2020-08-15 16:29:29 +0200

    Fix type for windows

--- daemon/platform/windows/process_helper.rs
+++ daemon/platform/windows/process_helper.rs
@@ -7,10 +7,12 @@ pub fn send_signal(pid: u32, action: &ProcessAction, children: bool) -> Result<b
     bail!("not supported on windows.")
 }
 
-/// Get all children of a specific process
+/// Get all children of a specific process.
+/// The Vec<i32> is just a placeholder.
 pub fn get_children(pid: i32) -> Option<Vec<i32>> {
     None
 }
 
-/// Send a signal to multiple processes
-pub fn send_signal_to_processes(pid: i32, action: &ProcessAction) {}
+/// Send a signal to multiple processes.
+/// The Vec<i32> is just a placeholder.
+pub fn send_signal_to_processes(processes: Vec<i32>, action: &ProcessAction) {}
