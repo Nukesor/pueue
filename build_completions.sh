a2e133e9d19a7ee8af1d050e7828f9332da2beff
Arne Beer <contact@arne.beer>
2020-10-27 00:11:24 +0100

    Enable zsh in build_completions

--- build_completions.sh
+++ build_completions.sh
@@ -3,5 +3,4 @@ cargo run --release --locked --bin pueue completions bash ./utils/completions
 cargo run --release --locked --bin pueue completions fish ./utils/completions
 cargo run --release --locked --bin pueue completions powershell ./utils/completions
 cargo run --release --locked --bin pueue completions elvish ./utils/completions
-# Broken for now -.-
-#cargo run --release --locked --bin pueue completions zsh ./utils/completions
+cargo run --release --locked --bin pueue completions zsh ./utils/completions
