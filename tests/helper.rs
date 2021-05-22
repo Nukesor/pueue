78b1a4e200be6d8c86aa8a019feaa5771daf8ecd
Arne Beer <contact@arne.beer>
2021-05-22 16:24:04 +0200

    Add get test config helper

--- tests/helper.rs
+++ tests/helper.rs
@@ -1,1 +1,59 @@
+use std::collections::BTreeMap;
 
+use tempdir::TempDir;
+
+use pueue_lib::settings::*;
+
+pub fn get_settings() -> (Settings, TempDir) {
+    // Create a temporary directory used for testing.
+    let tempdir = TempDir::new("pueue_lib").unwrap();
+    let tempdir_path = tempdir.path();
+
+    std::fs::create_dir(tempdir_path.join("certs")).unwrap();
+
+    let shared = Shared {
+        pueue_directory: tempdir_path.clone().to_path_buf(),
+        #[cfg(not(target_os = "windows"))]
+        use_unix_socket: true,
+        #[cfg(not(target_os = "windows"))]
+        unix_socket_path: tempdir_path.join("test.socket"),
+        host: "localhost".to_string(),
+        port: "51230".to_string(),
+        daemon_cert: tempdir_path.join("certs").join("daemon.cert"),
+        daemon_key: tempdir_path.join("certs").join("daemon.key"),
+        shared_secret_path: tempdir_path.join("secret"),
+    };
+
+    let client = Client {
+        read_local_logs: true,
+        show_confirmation_questions: false,
+        show_expanded_aliases: false,
+        dark_mode: false,
+        max_status_lines: Some(15),
+    };
+
+    let mut groups = BTreeMap::new();
+    groups.insert("default".to_string(), 1);
+    groups.insert("test".to_string(), 3);
+
+    let daemon = Daemon {
+        default_parallel_tasks: 1,
+        pause_group_on_failure: false,
+        pause_all_on_failure: false,
+        callback: None,
+        callback_log_lines: 15,
+        groups,
+    };
+
+    let settings = Settings {
+        client,
+        daemon,
+        shared,
+    };
+
+    settings
+        .save(&Some(tempdir_path.join("pueue.yml")))
+        .expect("Couldn't write pueue config to temporary directory");
+
+    (settings, tempdir)
+}
