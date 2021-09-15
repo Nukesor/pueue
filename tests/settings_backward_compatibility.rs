2f7863794659ce955fce2b030ffce26d5f5a2fd4
Arne Beer <contact@arne.beer>
2021-09-15 19:39:04 +0200

    tests: Ensure to add default group if not in config

--- tests/settings_backward_compatibility.rs
+++ tests/settings_backward_compatibility.rs
@@ -5,13 +5,13 @@ use anyhow::{Context, Result};
 use pueue_lib::settings::Settings;
 
 /// From 0.15.0 on, we aim to have full backward compatibility.
-/// For this reason, an old (slightly modified) v0.15.0 serialized state
+/// For this reason, an old (slightly modified) v0.15.0 serialized settings file
 /// has been checked in.
 ///
-/// We have to be able to restore from that state at all costs.
+/// We have to be able to restore from that config at all costs.
 /// Everything else results in a breaking change and needs a major version change.
 ///
-/// On top of simply having an old state, I also added a few non-existing fields.
+/// On top of simply having an old settings, I also removed a few default fields.
 /// This should be handled as well.
 #[test]
 fn test_restore_from_old_state() -> Result<()> {
@@ -21,9 +21,11 @@ fn test_restore_from_old_state() -> Result<()> {
         .join("data")
         .join("v0.15.0_settings.yml");
 
-    // Open v0.12.2 file and write old state to it.
-    let _: Settings = Settings::read_with_defaults(true, &Some(old_settings_path))
+    // Open v0.15.0 file and ensure the settings file can be read.
+    let settings: Settings = Settings::read_with_defaults(true, &Some(old_settings_path))
         .context("Failed to read old config with defaults:")?;
+
+    assert!(settings.daemon.groups.get("default").is_some());
 
     Ok(())
 }
