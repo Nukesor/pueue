b57179e4ca8459bcbdd8b6c9c4122e44e601e065
Arne Beer <contact@arne.beer>
2021-11-07 02:13:25 +0100

    refactor: Replace build.rs with Cargo's rust-version

--- build.rs
+++
@@ -1,9 +0,0 @@
-use ::version_check;
-
-fn main() {
-    if !version_check::is_min_version("1.42.0").unwrap_or(false) {
-        eprintln!("Pueue needs to be build with Rust version >=1.42");
-        eprintln!("Please update your rust version to stable.");
-        std::process::exit(1);
-    }
-}
