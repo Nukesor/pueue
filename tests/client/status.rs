96ab5dc60377b6548ab4882cb78eb67c7146029a
Arne Beer <contact@arne.beer>
2022-07-18 17:00:28 +0200

    tests Client 'status --json'

--- tests/client/status.rs
+++ tests/client/status.rs
@@ -1,4 +1,6 @@
+use anyhow::Context;
 use anyhow::Result;
+use pueue_lib::state::State;
 use rstest::rstest;
 
 use crate::fixtures::*;
@@ -70,6 +72,31 @@ async fn colored_status() -> Result<()> {
 
     let context = get_task_context(&daemon.settings).await?;
     assert_stdout_matches("status__status_with_color", output.stdout, context)?;
+
+    Ok(())
+}
+
+/// Calling `pueue status --json` will result in the current state being printed to the cli.
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn status_json() -> Result<()> {
+    let daemon = daemon().await?;
+    let shared = &daemon.settings.shared;
+
+    // Add a task and wait until it finishes.
+    assert_success(add_task(shared, "ls", false).await?);
+    wait_for_task_condition(shared, 0, |task| task.is_done()).await?;
+
+    let output = run_client_command(shared, &["status", "--json"]).await?;
+
+    let json = String::from_utf8_lossy(&output.stdout);
+    let deserialized_state: State =
+        serde_json::from_str(&*json).context("Failed to deserialize json state")?;
+
+    let state = get_state(shared).await?;
+    assert_eq!(
+        deserialized_state, *state,
+        "Json state differs from actual daemon state."
+    );
 
     Ok(())
 }
