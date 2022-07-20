fa59b871b4db958204a5b24cac567b949e3efdf1
Arne Beer <contact@arne.beer>
2022-07-20 02:34:37 +0200

    tests: Client remote follow with last lines

--- tests/client/follow.rs
+++ tests/client/follow.rs
@@ -51,3 +51,31 @@ async fn remote_follow() -> Result<()> {
 
     Ok(())
 }
+
+/// Test that the remote `follow` command works, if one specifies to only show the last few lines
+/// of recent output.
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn remote_follow_with_last_lines() -> Result<()> {
+    let mut daemon = daemon().await?;
+    let shared = &daemon.settings.shared;
+
+    // Force the client to read remote logs via config file.
+    daemon.settings.client.read_local_logs = false;
+    // Persist the change, so it can be seen by the client.
+    daemon
+        .settings
+        .save(&Some(daemon.tempdir.path().join("pueue.yml")))
+        .context("Couldn't write pueue config to temporary directory")?;
+
+    // Add a task which echos 8 lines of output
+    assert_success(add_task(shared, "echo \"1\n2\n3\n4\n5\n6\n7\n8\" && sleep 1", false).await?);
+    wait_for_task_condition(shared, 0, |task| task.is_running()).await?;
+
+    // Execute `follow`.
+    // This will result in the client receiving the streamed output until the task finished.
+    let output = run_client_command(shared, &["follow", "--lines=4"]).await?;
+
+    assert_stdout_matches("follow__last_lines", output.stdout, HashMap::new())?;
+
+    Ok(())
+}
