59b8785ce4c7d8df3d2c1dcc28ee4f8573279477
Arne Beer <contact@arne.beer>
2022-09-14 17:09:27 +0200

    tests: Default filter status

--- tests/client/status_filter.rs
+++ tests/client/status_filter.rs
@@ -5,7 +5,6 @@ use anyhow::Result;
 use chrono::{Duration, Local};
 use pueue_daemon_lib::state_helper::save_state;
 
-use pueue_lib::settings::Settings;
 use pueue_lib::state::{State, PUEUE_DEFAULT_GROUP};
 use pueue_lib::task::{Task, TaskResult, TaskStatus};
 
@@ -25,9 +24,15 @@ fn build_task() -> Task {
     )
 }
 
-/// Build and save a state with some pre-build tasks we can use to test our filters.
-/// The state is saved and created before the daemon is started.
-fn build_test_state(settings: &Settings) -> Result<()> {
+/// Initialize a daemon which already contains a predefined list of tasks in various states.
+async fn daemon_with_test_state() -> Result<PueueDaemon> {
+    // Get the base setup for the daemon
+    let (settings, tempdir) = daemon_base_setup()?;
+
+    // ------ Inert tasks -------
+    // Build and save a state with some pre-build tasks we can use to test our filters.
+    // The state is saved and created before the daemon is started.
+
     let mut state = State::new();
 
     // Failed task
@@ -46,18 +51,46 @@ fn build_test_state(settings: &Settings) -> Result<()> {
     successful.end = Some(Local::now() - Duration::days(2) + Duration::minutes(1));
     state.tasks.insert(successful.id, successful);
 
-    save_state(&state, settings)?;
+    // Stashed task
+    let mut successful = build_task();
+    successful.status = TaskStatus::Stashed { enqueue_at: None };
+    successful.id = 2;
+    state.tasks.insert(successful.id, successful);
 
-    Ok(())
+    // Scheduled task
+    let mut successful = build_task();
+    successful.status = TaskStatus::Stashed {
+        enqueue_at: Some(Local::now() + Duration::hours(1)),
+    };
+    successful.id = 3;
+    state.tasks.insert(successful.id, successful);
+
+    // Save the state in our temporary directory. This makes it readable by the daemon.
+    save_state(&state, &settings)?;
+
+    // ------ Daemon setup -------
+    // Start the daemon. It will restore the state we just saved.
+    let daemon = daemon_with_settings(settings, tempdir).await?;
+    let shared = &daemon.settings.shared;
+
+    // ------ Live tasks -------
+    // Now we have to add some tasks that need to be added live.
+
+    // Running task
+    assert_success(add_task(shared, "sleep 60", false).await?);
+
+    // 2 Queued tasks
+    assert_success(add_task(shared, "sleep 60", false).await?);
+    assert_success(add_task(shared, "sleep 60", false).await?);
+
+    Ok(daemon)
 }
 
-/// Test that the normal status command works as expected.
-/// Calling `pueue` without any subcommand is equivalent of using `status`.
+/// This is a default `status` call without any paramaters.
+/// This only exists to ensure the baseline behavior of our test state.
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-async fn column_filter() -> Result<()> {
-    let (settings, tempdir) = daemon_base_setup()?;
-    build_test_state(&settings)?;
-    let daemon = daemon_with_settings(settings, tempdir).await?;
+async fn filter_default() -> Result<()> {
+    let daemon = daemon_with_test_state().await?;
     let shared = &daemon.settings.shared;
 
     // Add a task and wait until it finishes.
@@ -66,7 +99,7 @@ async fn column_filter() -> Result<()> {
     let output = run_client_command(shared, &["status"]).await?;
 
     let context = get_task_context(&daemon.settings).await?;
-    assert_stdout_matches("status__default_status", output.stdout, context)?;
+    assert_stdout_matches("filter__default_status", output.stdout, context)?;
 
     Ok(())
 }
