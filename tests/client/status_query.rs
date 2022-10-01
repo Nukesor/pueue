2b04910fe3e6e999a5e2cd7ce77a93c974add036
Arne Beer <contact@arne.beer>
2022-10-01 21:57:12 +0200

    add: Label contains filter and label tests

--- tests/client/status_query.rs
+++ tests/client/status_query.rs
@@ -26,7 +26,7 @@ fn build_task() -> Task {
 }
 
 /// Initialize a daemon which already contains a predefined list of tasks in various states.
-async fn daemon_with_test_state() -> Result<PueueDaemon> {
+async fn daemon_with_test_state(with_label: bool) -> Result<PueueDaemon> {
     // Get the base setup for the daemon
     let (settings, tempdir) = daemon_base_setup()?;
 
@@ -42,6 +42,9 @@ async fn daemon_with_test_state() -> Result<PueueDaemon> {
     failed.status = TaskStatus::Done(TaskResult::Failed(255));
     failed.start = Some(Local::now() - Duration::days(1) + Duration::minutes(1));
     failed.end = Some(Local::now() - Duration::days(1) + Duration::minutes(5));
+    if with_label {
+        failed.label = Some("label-10-0".to_string());
+    }
     state.tasks.insert(failed.id, failed);
 
     // Successful task
@@ -50,21 +53,27 @@ async fn daemon_with_test_state() -> Result<PueueDaemon> {
     successful.status = TaskStatus::Done(TaskResult::Success);
     successful.start = Some(Local::now() - Duration::days(2) + Duration::minutes(1));
     successful.end = Some(Local::now() - Duration::days(2) + Duration::minutes(5));
+    if with_label {
+        successful.label = Some("label-10-1".to_string());
+    }
     state.tasks.insert(successful.id, successful);
 
     // Stashed task
-    let mut successful = build_task();
-    successful.status = TaskStatus::Stashed { enqueue_at: None };
-    successful.id = 2;
-    state.tasks.insert(successful.id, successful);
+    let mut stashed = build_task();
+    stashed.status = TaskStatus::Stashed { enqueue_at: None };
+    stashed.id = 2;
+    if with_label {
+        stashed.label = Some("label-10-2".to_string());
+    }
+    state.tasks.insert(stashed.id, stashed);
 
     // Scheduled task
-    let mut successful = build_task();
-    successful.status = TaskStatus::Stashed {
+    let mut scheduled = build_task();
+    scheduled.status = TaskStatus::Stashed {
         enqueue_at: Some(Local::now() + Duration::minutes(2)),
     };
-    successful.id = 3;
-    state.tasks.insert(successful.id, successful);
+    scheduled.id = 3;
+    state.tasks.insert(scheduled.id, scheduled);
 
     // Save the state in our temporary directory. This makes it readable by the daemon.
     save_state(&state, &settings)?;
@@ -92,7 +101,7 @@ async fn daemon_with_test_state() -> Result<PueueDaemon> {
 /// This only exists to ensure the baseline behavior of our test state.
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn default() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     let output = run_client_command(shared, &["status"])?;
@@ -106,7 +115,7 @@ async fn default() -> Result<()> {
 /// Select only specific columns for printing
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn column_selection() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     let output = run_client_command(shared, &["status", "columns=id,status,command"])?;
@@ -120,7 +129,7 @@ async fn column_selection() -> Result<()> {
 /// Select the first few entries of the list
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn limit_first() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     let output = run_client_command(shared, &["status", "first 4"])?;
@@ -134,7 +143,7 @@ async fn limit_first() -> Result<()> {
 /// Select the first few entries of the list
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn limit_last() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     let output = run_client_command(shared, &["status", "last 4"])?;
@@ -148,7 +157,7 @@ async fn limit_last() -> Result<()> {
 /// Order the test state by task status.
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn order_by_status() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     let output = run_client_command(shared, &["status", "order_by status"])?;
@@ -162,7 +171,7 @@ async fn order_by_status() -> Result<()> {
 /// Filter by start date
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn filter_start() -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     // Filter tasks by their start time. This includes only task 0, which was started 1 day ago.
@@ -181,7 +190,7 @@ async fn filter_start() -> Result<()> {
 #[case("%F")]
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn filter_end_with_time(#[case] format: &'static str) -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     // Filter tasks by their end time, once by day (today) and time (now).
@@ -195,7 +204,7 @@ async fn filter_end_with_time(#[case] format: &'static str) -> Result<()> {
     Ok(())
 }
 
-/// Filter for tasks
+/// Filter tasks by status
 #[rstest]
 #[case(TaskStatus::Queued, 2)]
 #[case(TaskStatus::Running, 1)]
@@ -203,8 +212,8 @@ async fn filter_end_with_time(#[case] format: &'static str) -> Result<()> {
 #[case(TaskStatus::Done(TaskResult::Success), 1)]
 #[case(TaskStatus::Done(TaskResult::Failed(255)), 1)]
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-async fn filter_status(#[case] status: TaskStatus, #[case] expected: usize) -> Result<()> {
-    let daemon = daemon_with_test_state().await?;
+async fn filter_status(#[case] status: TaskStatus, #[case] match_count: usize) -> Result<()> {
+    let daemon = daemon_with_test_state(false).await?;
     let shared = &daemon.settings.shared;
 
     // Get the correct query keyword for the given status.
@@ -223,20 +232,77 @@ async fn filter_status(#[case] status: TaskStatus, #[case] expected: usize) -> R
         .await
         .expect("Failed to get json from task");
 
-    let mut count = 0;
-    for task in tasks {
+    for task in tasks.iter() {
         let id = task.id;
         assert_eq!(
             task.status, status,
             "Expected a different task status on task {id} based on filter {status:?}"
         );
-
-        count += 1;
     }
 
     assert_eq!(
-        count, expected,
+        tasks.len(),
+        match_count,
         "Got a different amount of tasks than expected for the status filter {status:?}."
+    );
+
+    Ok(())
+}
+
+/// Filter tasks by label with the "contains" `%=` filter.
+#[rstest]
+#[case("%=", "label", 3)]
+#[case("%=", "label-10", 3)]
+#[case("%=", "label-10-1", 1)]
+#[case("=", "label-10-1", 1)]
+#[case("!=", "label-10-1", 6)]
+#[case("!=", "label-10", 7)]
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn filter_label(
+    #[case] operator: &'static str,
+    #[case] label_filter: &'static str,
+    #[case] match_count: usize,
+) -> Result<()> {
+    let daemon = daemon_with_test_state(true).await?;
+    let shared = &daemon.settings.shared;
+
+    let query = format!("label{operator}{label_filter}");
+    let tasks = get_json_tasks_from_command(shared, &[query.as_str()])
+        .await
+        .expect("Failed to get json from task");
+
+    for task in tasks.iter() {
+        // Make sure the task either has no label or the label doesn't match the filter.
+        if operator == "!=" {
+            if let Some(label) = &task.label {
+                assert_ne!(
+                    label, label_filter,
+                    "Label '{label}' matched exact filter '{label_filter}'"
+                );
+            }
+            continue;
+        }
+
+        let label = task.label.as_ref().expect("Expected task to have a label");
+        if operator == "%=" {
+            // Make sure the label contained our filter.
+            assert!(
+                label.contains(&label_filter),
+                "Label '{label}' didn't contain filter '{label_filter}'"
+            );
+        } else if operator == "=" {
+            // Make sure the label exactly matches the filter.
+            assert_eq!(
+                label, &label_filter,
+                "Label '{label}' didn't match exact filter '{label_filter}'"
+            );
+        }
+    }
+
+    assert_eq!(
+        tasks.len(),
+        match_count,
+        "Got a different amount of tasks than expected for the label filter: {label_filter}."
     );
 
     Ok(())
