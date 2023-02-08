aa6ec4beaad10df020be9bd9c87997dd72b11154
Martijn Pieters <mj@zopatista.com>
2023-02-08 16:31:38 +0000

    Use new libproc functionality to list group pids
    Replace darwin-libproc use with libproc::processes::pids_by_type(),
    which is cross-platform. This returns pids _other than the group leader_,
    so adjust the expected counts in tests down by one.

--- pueue_lib/src/process_helper/apple.rs
+++ pueue_lib/src/process_helper/apple.rs
@@ -4,21 +4,3 @@ use libproc::libproc::{proc_pid, task_info};
 pub fn process_exists(pid: u32) -> bool {
     proc_pid::pidinfo::<task_info::TaskInfo>(pid.try_into().unwrap(), 0).is_ok()
 }
-
-#[cfg(test)]
-pub mod tests {
-    // TODO: swap darwin_libproc out for libproc when that project supports listing pids by
-    // group id.
-    use darwin_libproc;
-    use log::warn;
-
-    pub fn get_process_group_pids(pgrp: i32) -> Vec<i32> {
-        match darwin_libproc::pgrp_only_pids(pgrp) {
-            Err(error) => {
-                warn!("Failed to get list of processes in process group {pgrp}: {error}");
-                Vec::new()
-            }
-            Ok(processes) => processes,
-        }
-    }
-}
