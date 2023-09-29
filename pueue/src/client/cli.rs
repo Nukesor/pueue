52c6881d0be58b254eeb29d43384a018b56d03f2
Arne Beer <contact@arne.beer>
2023-09-29 11:02:56 +0200

    add: PUEUE_CONFIG env variable support

--- pueue/src/client/cli.rs
+++ pueue/src/client/cli.rs
@@ -537,8 +537,9 @@ pub struct CliArguments {
     #[arg(long, value_enum, default_value = "auto")]
     pub color: ColorChoice,
 
-    /// Path to a specific pueue config file to use.
-    /// This ignores all other config files.
+    /// If provided, Pueue only uses this config file.
+    /// This path can also be set via the "PUEUE_CONFIG_PATH" environment variable.
+    /// The commandline option overwrites the environment variable!
     #[arg(short, long, value_hint = ValueHint::FilePath)]
     pub config: Option<PathBuf>,
 
