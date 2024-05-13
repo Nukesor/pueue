60e130eb881caf1d9390597542add75adeacc2d2
Ryan Steinmetz <zi@FreeBSD.org>
2024-05-13 18:56:55 -0400

    Add FreeBSD process helper support

---
+++ pueue_lib/src/process_helper/freebsd.rs
@@ -0,0 +1,15 @@
+use std::path::Path;
+
+/// Check, whether a specific process is exists or not
+pub fn process_exists(pid: u32) -> bool {
+    return Path::new(&format!("/proc/{}", pid)).is_dir();
+}
+
+#[cfg(test)]
+pub mod tests {
+    /// Get all processes in a process group
+    pub fn get_process_group_pids(pgrp: i32) -> Vec<i32> {
+        /// TODO
+	return { };
+    }
+}
--- pueue_lib/src/process_helper/mod.rs
+++ pueue_lib/src/process_helper/mod.rs
@@ -21,6 +21,7 @@ use command_group::Signal;
 #[cfg_attr(target_os = "linux", path = "linux.rs")]
 #[cfg_attr(target_vendor = "apple", path = "apple.rs")]
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
+#[cfg_attr(target_os = "freebsd", path = "freebsd.rs")]
 mod platform;
 pub use self::platform::*;
 
