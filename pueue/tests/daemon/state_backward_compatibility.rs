08cdcd3067ac706295d9d3290ca9893cfdd56229
Arne Beer <contact@arne.beer>
2024-06-16 09:28:28 +0200

    fix: Some comments and add unknown fields to config dserialization test

--- pueue/tests/daemon/state_backward_compatibility.rs
+++ pueue/tests/daemon/state_backward_compatibility.rs
@@ -7,8 +7,9 @@ use tempfile::TempDir;
 
 use pueue_lib::settings::Settings;
 
-/// From 0.12.2 on, we aim to have full backward compatibility.
-/// For this reason, an old v0.12.2 serialized state has been checked in.
+/// 2.0.0 introduced some breaking changes.
+/// From here on, we now aim to once again have full backward compatibility.
+/// For this reason, an old v2.0.0 serialized state has been checked in.
 ///
 /// We have to be able to restore from that state at all costs.
 /// Everything else results in a breaking change and needs a major version change.
